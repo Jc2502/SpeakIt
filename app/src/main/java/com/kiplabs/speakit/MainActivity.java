374edc03a8fdba6012966a1fc1f5f0c37c8ca00e
JUAN-C <juank2acosta@gmail.com>
2015-07-06 11:42:20 -0500

    My First Commit

---
+++ app/src/main/java/com/kiplabs/speakit/MainActivity.java
@@ -0,0 +1,22 @@
+package com.kiplabs.speakit;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
